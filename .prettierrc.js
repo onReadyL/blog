aa040b5dc151ad14edc6294f414b58b6d38a8b78
liuxiaobin <xiaobin.liu@karakal.com.cn>
2021-04-27 14:39:14 +0800

    feat: axios取消重复请求

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,19 +1,22 @@
 module.exports = {
-	singleQuote: true, // 禁用单引号
-	jsxSingleQuote: false, // 使用单引号
-	trailingComma: 'all',
-	printWidth: 100,
+	singleQuote: true, // 单引号
+	jsxSingleQuote: true, // 使用单引号
+	trailingComma: 'all', // 尾逗号
+	printWidth: 100, // 单行最大长度限制
 	semi: true, //行尾是否使用分号，默认为true
-	bracketSpacing: true,
+	bracketSpacing: true, // 对象字面量括号前后空格
 	useTabs: true, //是否使用tab进行缩进，默认为false，表示用空格进行缩减
-	//tabWidth: 2, //一个tab代表几个空格数，默认为80
+	tabWidth: 2, //一个tab代表几个空格数，默认为80
 	jsxBracketSameLine: true, // 启用jsx语法，> 放在末尾
 	overrides: [
 		{
 			files: '.prettierrc',
 			options: {
-				parser: 'json'
-			}
-		}
-	]
+				parser: 'json',
+			},
+		},
+	],
+	requirePragma: false,
+	insertPragma: false,
+	endOfLine: 'auto',
 };
